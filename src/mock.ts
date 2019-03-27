8b70e7406cac87f75f83ddff97b8f21788e6e25b
BRUNSWICK\jmad0204 <jayden.maddison@merchantcantos.com>
2019-03-27 12:19:25 +0000

    graphql mock setup

---
+++ src/mock.ts
@@ -0,0 +1,39 @@
+import { SchemaLink } from 'apollo-link-schema';
+import { addMockFunctionsToSchema, makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+type Query {
+    commentaries: [Commentary]
+}
+type Commentary {
+  id: ID!,
+  title: String!,
+  text: String!,
+  minute: Number!
+}
+`;
+
+const commentaries = [{
+  id: 1001,
+  title: 'This is the second commentary',
+  text: 'This is the first commentary',
+  minute: 12
+}, {
+  id: 1002,
+  title: 'This is the second commentary',
+  text: 'This is the second commentary',
+  minute: 17
+}];
+
+const mocks = {
+  Query: () => ({
+    commentaries: () => commentaries,
+  }),
+};
+
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ mocks, schema });
+
+export const mockedLink = new SchemaLink({ schema });
+
+export default null;
